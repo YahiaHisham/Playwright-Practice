81026705203df2a414650653bfd36509d4f831fa
YahiaElAydyRasan <yhisham@rasan.co>
2024-05-15 15:37:28 +0300

    test

--- src/main/java/pages/SignupLoginPage.java
+++ src/main/java/pages/SignupLoginPage.java
@@ -3,16 +3,34 @@ package pages;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
-public class SignupLoginPage extends PageBase{
+public class SignupLoginPage extends PageBase {
     Locator signupName = page.locator("input[name='name']");
     Locator signupMail = page.locator("input[data-qa='signup-email']");
     Locator signupButton = page.locator("button[data-qa='signup-button']");
+
     public SignupLoginPage(Page page) {
         super(page);
     }
-    public void setSignupName(){
+
+    public SignupLoginPage setSignupName() {
         setElementText(signupName, "Yahia Hisham");
+
+        return this;
+    }
+
+    public SignupLoginPage setSignupMail() {
         setElementText(signupMail, "yahia@mail.com");
+        return this;
+    }
+
+    public SignupLoginPage clickOnSignupButton() {
         clickOnElement(signupButton);
+        return this;
+    }
+
+    public void enterSignupNameAndEmail() {
+        setSignupName();
+        setSignupMail();
+        clickOnSignupButton();
     }
 }
--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,5 +1,6 @@
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.SignupLoginPage;
 
 public class TestClass extends TestBase {
 
@@ -7,6 +8,13 @@ public class TestClass extends TestBase {
     @Test
     public void firstTestCase() {
         new HomePage(page).clickOnSignupButton();
+        new SignupLoginPage(page).enterSignupNameAndEmail();
+    }
+
+    @Test
+    public void birstTestCase() {
+        new HomePage(page).clickOnSignupButton();
+        new SignupLoginPage(page).enterSignupNameAndEmail();
     }
 
 }
